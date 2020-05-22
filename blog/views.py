87a0921a17fa72810ff477bb6c91552e964c4894
sujay <sujay_shekhar@outlook.com>
2020-05-22 23:46:30 +0530

    migrations

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return render(request,'blog/blogHome.html')
+
+def blogPost(request,slug):
+    return render(request,'blog/blogPost.html')
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'home/home.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+def about(request):
+    return render(request, 'home/about.html')
