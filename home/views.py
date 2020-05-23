de46b8c2b3970db0564cd4614ed3d6be72c7ddee
sujay <sujay_shekhar@outlook.com>
2020-05-23 22:18:12 +0530

    contactform and Post request

--- home/views.py
+++ home/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Contact
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
 
 def contact(request):
+    if request.method=='POST':
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        content = request.POST['content']
+        print(name,email,phone,content)
+        contact = Contact(name=name,phone=phone,email=email,content=content)
+        contact.save()
     return render(request, 'home/contact.html')
 
 def about(request):
--- iProgrammer/settings.py
+++ iProgrammer/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
+    'home.apps.HomeConfig',
     
 ]
 
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
