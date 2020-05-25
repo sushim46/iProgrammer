23ac283be78a1ea13bdcecb66ae51ebc23d7a65e
sujay <sujay_shekhar@outlook.com>
2020-05-25 23:06:14 +0530

    about

--- home/views.py
+++ home/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from .models import Contact
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
 
 def contact(request):
+    messages.success(request,'Fill This Form To Contact Me. ')
     if request.method=='POST':
         name = request.POST['name']
         phone = request.POST['phone']
         email = request.POST['email']
         content = request.POST['content']
-        print(name,email,phone,content)
-        contact = Contact(name=name,phone=phone,email=email,content=content)
-        contact.save()
+        # print(name,email,phone,content)
+        if len(name)<3 or len(email)<4 or len(phone)<10 or len(content)<10:
+            messages.error(request, 'Please Fill The Form Correctly !!')
+        else:
+            contact = Contact(name=name,phone=phone,email=email,content=content)
+            contact.save()
+            messages.success(request,f'{name} Thank You For Contacting Us. Your Message Has Been Sent Succesfully. ')
+       
     return render(request, 'home/contact.html')
 
 def about(request):
--- iProgrammer/settings.py
+++ iProgrammer/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+MESSAGE_TAGS = {
+    messages.ERROR : 'danger'
+}
